020db23cacef472a255692ab7ae394f087d4bc26
Torr7s <torresmiranda.rj@gmail.com>
2022-07-01 23:09:06 -0300

    refactor(transactions): import use cases

--- src/modules/transactions/transactions.module.ts
+++ src/modules/transactions/transactions.module.ts
@@ -2,10 +2,12 @@ import { forwardRef, Module } from '@nestjs/common';
 
 import { PrismaService } from '@shared/prisma/service/prisma.service';
 
+import { CreateTransactionController } from '@modules/transactions/useCases/create/create-transaction.controller';
 import { FindTransactionController } from './useCases/find/find-transaction.controller';
 import { ListTransactionsController } from './useCases/list/list-transactions.controller';
 import { UpdateTransactionController } from '@modules/transactions/useCases/update/update-transaction.controller';
 
+import { CreateTransactionUseCase } from '@modules/transactions/useCases/create/create-transaction.use-case';
 import { FindTransactionUseCase } from './useCases/find/find-transaction.use-case';
 import { ListTransactionsUseCase } from './useCases/list/list-transactions.use-case';
 import { UpdateTransactionUseCase } from '@modules/transactions/useCases/update/update-transaction.use-case';
@@ -19,12 +21,14 @@ import { PayablesModule } from '@modules/payables/payables.module';
     forwardRef(() => PayablesModule)
   ],
   controllers: [
+    CreateTransactionController,
     FindTransactionController,
     ListTransactionsController,
     UpdateTransactionController,
   ],
   providers: [
     PrismaService,
+    CreateTransactionUseCase,
     FindTransactionUseCase,
     ListTransactionsUseCase,
     UpdateTransactionUseCase,
