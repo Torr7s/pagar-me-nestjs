9db256ad7d77c1b1c900060965bfd4dbde670dde
Torr7s <torresmiranda.rj@gmail.com>
2022-07-03 12:07:22 -0300

    docs(consumers): finalize documentation for ConsumersModule

--- src/modules/consumers/useCases/find/find-consumer.controller.ts
+++ src/modules/consumers/useCases/find/find-consumer.controller.ts
@@ -4,14 +4,32 @@ import {
   Get,
   Param, Res
 } from '@nestjs/common';
+import {
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+} from '@nestjs/swagger';
+
+import { CreateConsumerResponse } from '@modules/consumers/domain/swagger/responses/create-consumer.response';
 
 import { FindConsumerUseCase } from './find-consumer.use-case';
 
 import { Consumers } from '@prisma/client';
 
+import { ApiTagsAndBearer } from '@shared/utils/decorators/nest.decorators';
+
+@ApiTagsAndBearer('Consumers')
 @Controller('/api/consumers')
 export class FindConsumerController {
   constructor(private findConsumerUseCase: FindConsumerUseCase) {};
+  
+  @ApiParam({
+    name: 'id',
+    type: 'string'
+  })
+  @ApiOperation({ description: 'Find a consumer profile.' })
+  @ApiResponse({ status: 200, description: 'A consumer profile was found.', type: CreateConsumerResponse })
+  @ApiResponse({ status: 400, description: 'Consumer does not exists.' })
   
   @Get('/:id')
   async handle(
--- src/modules/consumers/useCases/list/list-consumers.controller.ts
+++ src/modules/consumers/useCases/list/list-consumers.controller.ts
@@ -4,17 +4,35 @@ import {
   Get, 
   Req, Res
 } from '@nestjs/common';
+import {
+  ApiOperation,
+  ApiResponse,
+} from '@nestjs/swagger';
+
+import { CreateConsumerResponse } from '@modules/consumers/domain/swagger/responses/create-consumer.response';
 
 import { ListConsumersUseCase } from './list-consumers.use-case';
 
 import { Consumers } from '@prisma/client';
 
+import { ApiTagsAndBearer } from '@shared/utils/decorators/nest.decorators';
+
+@ApiTagsAndBearer('Consumers')
 @Controller('/api/consumers')
 export class ListConsumersController {
   constructor(private listConsumersUseCase: ListConsumersUseCase) {};
-
+  
+  @ApiOperation({ description: 'List all registered consumers. '})
+  @ApiResponse({
+    status: 200,
+    description: 'At least one consumer profile was found.',
+    isArray: true,
+    type: CreateConsumerResponse
+  })
+  @ApiResponse({ status: 400, description: 'No consumer profiles found.' })
+  
   @Get('/')
-  async handle(
+    async handle(
     @Req() request: Request,
     @Res() response: Response
   ): Promise<Response> {
