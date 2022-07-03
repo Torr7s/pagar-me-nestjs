30698ce90c2c66bc20119c6517e942f127f0fe9f
Torr7s <torresmiranda.rj@gmail.com>
2022-07-02 23:44:53 -0300

    docs: setup SwaggerModule

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
+import { SwaggerModule, DocumentBuilder, OpenAPIObject } from '@nestjs/swagger';
+
 import { PrismaService } from '@shared/prisma/service/prisma.service';
 
 (async (): Promise<void> => {
@@ -12,6 +14,21 @@ import { PrismaService } from '@shared/prisma/service/prisma.service';
     whitelist: true,
     forbidNonWhitelisted: true
   }));
+  
+  const config: Omit<OpenAPIObject, "paths"> = new DocumentBuilder()
+    .setTitle('Swagger Documentation - PagarMe')
+    .setDescription(
+      'This documentation will list all the functionalities, routes and features of the application.'
+    )
+    .setVersion('3.4.0')
+    .addTag('Consumers')
+    .addTag('Payables')
+    .addTag('Transactions')
+    .build();
+  
+  const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
+  
+  SwaggerModule.setup('api', app, document);
   
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
