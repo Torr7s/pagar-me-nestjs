b323f52c2594483b78e49d27f9703c7f953bd4f7
Torr7s <torresmiranda.rj@gmail.com>
2022-07-04 19:04:36 -0300

    docs(payables): setup ListPayablesController docs

--- src/modules/payables/useCases/list/list-payables.controller.ts
+++ src/modules/payables/useCases/list/list-payables.controller.ts
@@ -4,13 +4,37 @@ import {
   Get,
   Param, Res
 } from '@nestjs/common';
+import {
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+} from '@nestjs/swagger';
 
 import { ListPayablesUseCase } from '@modules/payables/useCases/list/list-payables.use-case';
+import { ListPayablesResponse } from '@modules/payables/domain/swagger/responses/list-payables.response';
+
 import { Payables } from '@prisma/client';
 
+import { ApiTagsAndBearer } from '@shared/utils/decorators/nest.decorators';
+
+@ApiTagsAndBearer('Payables')
 @Controller('/api/payables')
 export class ListPayablesController {
   constructor(private listPayablesUseCase: ListPayablesUseCase) {};
+  
+  @ApiParam({
+    name: 'status',
+    type: 'string'
+  })
+  @ApiOperation({ description: 'List all payables by their status.' })
+  @ApiResponse({
+    status: 200,
+    description: 'Payables successfully found.',
+    isArray: true,
+    type: ListPayablesResponse
+  })
+  @ApiResponse({ status: 400, description: 'No payables were found.' })
+  @ApiResponse({ status: 404, description: 'Incorrect or misreported availability status.' })
   
   @Get('/:status')
   async handle(
