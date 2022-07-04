858ad95f07910caf0b3123434aeb1974b5703781
Torr7s <torresmiranda.rj@gmail.com>
2022-07-04 18:54:32 -0300

    refactor(modules/payables): add id property

--- src/modules/payables/infra/prisma/entities/payables.entity.ts
+++ src/modules/payables/infra/prisma/entities/payables.entity.ts
@@ -1,6 +1,7 @@
 import { Prisma } from '@prisma/client';
 
 export class PayablesEntity implements Prisma.PayablesUncheckedCreateInput {
+  id?: string;
   status?: string;
   payment_date?: string | Date;
   fee?: string | Prisma.Decimal;
