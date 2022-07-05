ba93dc25d2dbdfa234ac856de4d5ec0a06d7ec5c
Torr7s <torresmiranda.rj@gmail.com>
2022-07-05 16:01:31 -0300

    docs(transactions): setup and add FindTransactionController to docs

--- src/modules/transactions/useCases/find/find-transaction.controller.ts
+++ src/modules/transactions/useCases/find/find-transaction.controller.ts
@@ -4,14 +4,32 @@ import {
   Get,
   Param, Res
 } from '@nestjs/common';
+import {
+  ApiParam,
+  ApiOperation,
+  ApiResponse
+} from '@nestjs/swagger';
+
+import { CreateTransactionResponse } from '@modules/transactions/domain/swagger/responses/create-transaction.response';
 
 import { FindTransactionUseCase } from './find-transaction.use-case';
 
 import { Transactions } from '@prisma/client';
 
+import { ApiTagsAndBearer } from '@shared/utils/decorators/nest.decorators';
+
+@ApiTagsAndBearer('Transactions')
 @Controller('/api/transactions')
 export class FindTransactionController {
   constructor(private findTransactionUseCase: FindTransactionUseCase) {};
+  
+  @ApiParam({
+    name: 'id',
+    type: 'string'
+  })
+  @ApiOperation({ description: 'Find a transaction by its id.' })
+  @ApiResponse({ status: 200, description: 'Transaction successfully found.', type: CreateTransactionResponse })
+  @ApiResponse({ status: 400, description: 'No transaction was found.' })
   
   @Get('/:id')
   async handle(
