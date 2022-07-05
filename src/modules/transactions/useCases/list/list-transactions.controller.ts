a09883c545bac9e73dda6428df4a34ce1aa98010
Torr7s <torresmiranda.rj@gmail.com>
2022-07-05 16:05:02 -0300

    docs(transactions): add last TransactionsModule controller

--- src/modules/transactions/useCases/list/list-transactions.controller.ts
+++ src/modules/transactions/useCases/list/list-transactions.controller.ts
@@ -4,14 +4,32 @@ import {
   Get,
   Req, Res
 } from '@nestjs/common';
+import {
+  ApiParam,
+  ApiOperation,
+  ApiResponse
+} from '@nestjs/swagger';
 
 import { ListTransactionsUseCase } from './list-transactions.use-case';
 
 import { Transactions } from '@prisma/client';
 
+import { ApiTagsAndBearer } from '@shared/utils/decorators/nest.decorators';
+import { CreateTransactionResponse } from '@modules/transactions/domain/swagger/responses/create-transaction.response';
+
+@ApiTagsAndBearer('Transactions')
 @Controller('/api/transactions')
 export class ListTransactionsController {
   constructor(private listTransactionsUseCase: ListTransactionsUseCase) {};
+
+  @ApiOperation({ description: 'List all consumer transactions.' })
+  @ApiResponse({
+    status: 200,
+    description: 'Transactions successfully found.',
+    isArray: true,
+    type: CreateTransactionResponse
+  })
+  @ApiResponse({ status: 400, description: 'No transactions were found.' })
   
   @Get('/')
   async handle(
