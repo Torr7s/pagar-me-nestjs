f3bb183f275a2e1d859e27d4c4814665ea15fbcf
Torr7s <torresmiranda.rj@gmail.com>
2022-07-05 14:51:23 -0300

    docs(consumers): setup and add CreateTransactionController to docs

--- src/modules/transactions/useCases/create/create-transaction.controller.ts
+++ src/modules/transactions/useCases/create/create-transaction.controller.ts
@@ -4,13 +4,27 @@ import {
   Controller,
   Post, Req, Res
 } from '@nestjs/common';
+import {
+  ApiBody,
+  ApiOperation,
+  ApiResponse
+} from '@nestjs/swagger';
 
 import { CreateTransactionDto } from '@modules/transactions/domain/dtos/create-transaction.dto';
+import { CreateTransactionResponse } from '@modules/transactions/domain/swagger/responses/create-transaction.response';
 import { CreateTransactionUseCase } from './create-transaction.use-case';
 
+import { ApiTagsAndBearer } from '@shared/utils/decorators/nest.decorators';
+
+@ApiTagsAndBearer('Transactions')
 @Controller('/api/transactions')
 export class CreateTransactionController {
   constructor(private createTransactionUseCase: CreateTransactionUseCase) {};
+  
+  @ApiBody({ type: CreateTransactionDto })
+  @ApiOperation({ description: 'Create a transactions.' })
+  @ApiResponse({ status: 200, description: 'Transaction successfully created.', type: CreateTransactionResponse })
+  @ApiResponse({ status: 500, description: 'Internal Server Error.' })
   
   @Post('/')
   async handle(
